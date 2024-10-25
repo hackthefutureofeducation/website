33424d3d2302502b1fd8d1f1ccf96d846bfd5e50
Bashamega <adambashaahmednaji@gmail.com>
2024-10-25 14:04:05 +0300

    -

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,10 @@
 export function About() {
   return (
-    <div className="bg-gray-100 p-5 rounded-lg shadow-md lg:mx-[5%] my-[100px]" id="About">
+    <section className="p-5 rounded-lg shadow-md lg:mx-[5%] my-[100px]" id="About">
       <h1 className="text-3xl font-bold text-center mb-4 deviders">About Us</h1>
       <p className="text-gray-700 text-lg">
         I am very passionate about learning something new and challenging things, so I always try to keep growing and improving my skills.
       </p>
-    </div>
+    </section>
   );
 }
